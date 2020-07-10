742ac1c916e74b1c27d718b76bc08965b0214e6b
T. Isaac Lightburn <isaac@hozed.net>
2020-07-10 17:44:08 -0500

    Update openkj-unstable.rb

--- Formula/openkj-unstable.rb
+++ Formula/openkj-unstable.rb
@@ -8,6 +8,10 @@ class OpenkjUnstable < Formula
   license "GPLv3"
   depends_on "qt5"
   depends_on "gstreamer"
+  depends_on "gst-plugins-good"
+  depends_on "gst-plugins-bad"
+  depends_on "gst-plugins-ugly"
+  depends_on "gst-plugins-base"
   depends_on "taglib"
   depends_on "pkg-config"
   # depends_on "cmake" => :build
